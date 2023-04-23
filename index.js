1e3cc3bdb9ce6d38adf2fcf9e699cba54f480018
melissamariano <melissams538@gmail.com>
2023-04-23 08:39:45 -0400

    filter method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(names, string) {
+    return names.filter(names => names.toUpperCase() === string.toUpperCase());
+
+}
+
+function fuzzyMatch(names, string){
+    return names.filter(names => names[0] === string[0])
+}
+
+function matchName(name, hometown){
+    return name.filter(name => name.name === hometown);
+
+}
